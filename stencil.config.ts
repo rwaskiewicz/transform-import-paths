d994a36a9c001cea5ba8a2d1eaba8ec50b1bb3b7
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-06-15 09:46:31 -0400

    simplify

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,17 +2,8 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'create-elm',
+  transformAliasedImportPaths: true,
   outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements',
-    },
-    {
-      type: 'docs-readme',
-    },
     {
       type: 'www',
       serviceWorker: null, // disable service workers
